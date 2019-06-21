f5843b4c9d702c4be15c7f1796950b643d3ddbc5
nevilpaul <nevilross2@gmail.com>
2019-06-21 11:46:21 +0300

    src add

---
+++ src/App.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    // this.canvasCreatePreview = React.createRef();
+    this.state = {
+        imagefile:"",
+        imageResult:""
+    }
+}
+  handle_over = event => {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+  handle_enter = event => {
+      event.preventDefault();
+      event.stopPropagation();
+      // event.target.parentNode.children[1].style.display = "none";
+      event.target.parentNode.children[1].children[0] !== undefined ? event.target.parentNode.children[1].children[0].style.display = "none":console.log('no child')
+      
+  }
+  handle_drop = event => {
+      event.preventDefault();
+      event.stopPropagation();
+      var dt = event.dataTransfer;
+      var file = dt.files[0];
+      this.setState({
+          imagefile: file
+      });
+      var fileLength = dt.files.length;
+ 
+      if (file.type.startsWith("image/") && fileLength === 1) {
+          // var image = document.querySelector('.ReactCrop__image');
+          var reader = new FileReader();
+          reader.addEventListener('load', ()=> {
+              this.setState({
+                imageResult: reader.result
+              });
+          },false);
+          reader.readAsDataURL(file);
+      }
+  }
+  clicked_event = event =>{
+    event.preventDefault();
+    let clicker = document.getElementById('dragImage');
+    var dt = event.target;
+    console.log(dt)
+    var file = dt.files[0];
+    console.log(file)
+    this.setState({
+      imagefile: file
+    });
+    var fileLength = dt.files.length;
+ 
+    if (file.type.startsWith("image/") && fileLength === 1) {
+      // var image = document.querySelector('.ReactCrop__image');
+      var reader = new FileReader();
+      reader.addEventListener('load', ()=> {
+        clicker.style.display ="none";
+        this.setState({
+          imageResult: reader.result
+        });
+      },false);
+      reader.readAsDataURL(file);
+    }
+
+  }
+  clickImageChanger =e=>{
+    let clicked = document.getElementById('imageUpload');
+    clicked.click();
+  }
+  render(){
+    const {imageResult} = this.state;
+    return (
+      <div className="App">
+      <h2>Image Classifier | php_ml</h2>
+        <div className="place" onDragOver={this.handle_over.bind(this)} onDragEnter={this.handle_enter.bind(this)} onDrop={this.handle_drop.bind(this)}>
+          {
+            imageResult === '' ?"":<img src = {imageResult} alt="" className="imageScaler"/>
+          }
+          <div className="dragImage" id="dragImage" onClick={this.clickImageChanger.bind(this)}>
+              <p>Select Image to upload Or drag and drop video files</p>
+          </div>
+          <input className="file" type="file" name="imageUpload" id="imageUpload" onChange={this.clicked_event.bind(this)}/>
+        </div>
+        <button className="name" type="button" value="submition">Classify</button>
+      </div>
+    );
+  }
+  
+}
+
+export default App;
