13ac1ca3e753c78a6fe9aa679eacb0a4b27ce871
nevilpaul <nevilross2@gmail.com>
2019-06-21 13:41:05 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ml5 from "ml5"
 import './App.css';
 
 class App extends React.Component {
@@ -71,6 +72,20 @@ class App extends React.Component {
     let clicked = document.getElementById('imageUpload');
     clicked.click();
   }
+  classify_image = event =>{
+    // Initialize the Image Classifier method with MobileNet
+  const classifier = ml5.imageClassifier('MobileNet', modelLoaded);
+
+  // When the model is loaded
+  function modelLoaded() {
+    console.log('Model Loaded!');
+  }
+
+  // Make a prediction with a selected image
+  classifier.predict(document.getElementById('image'), function(err, results) {
+    console.log(results);
+  });
+  }
   render(){
     const {imageResult} = this.state;
     return (
@@ -78,14 +93,14 @@ class App extends React.Component {
       <h2>Image Classifier | php_ml</h2>
         <div className="place" onDragOver={this.handle_over.bind(this)} onDragEnter={this.handle_enter.bind(this)} onDrop={this.handle_drop.bind(this)}>
           {
-            imageResult === '' ?"":<img src = {imageResult} alt="" className="imageScaler"/>
+            imageResult === '' ?"":<img src = {imageResult} alt="" className="imageScaler" id="image"/>
           }
           <div className="dragImage" id="dragImage" onClick={this.clickImageChanger.bind(this)}>
               <p>Select Image to upload Or drag and drop video files</p>
           </div>
           <input className="file" type="file" name="imageUpload" id="imageUpload" onChange={this.clicked_event.bind(this)}/>
         </div>
-        <button className="name" type="button" value="submition">Classify</button>
+        <button className="name" type="button" value="submition" onClick={this.classify_image.bind(this)}>Classify</button>
       </div>
     );
   }
