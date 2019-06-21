3362fa6583180c58615c1997603dba1573b624ef
nevilpaul <nevilross2@gmail.com>
2019-06-21 11:43:52 +0300

    update sumit

---
+++ build/precache-manifest.d546e469945209383e6dc991aecfafe0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ddb8c6f341f1db9693cb55f3761d824",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc5c91973cff72f54075",
+    "url": "/static/css/main.5087e6e2.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "fc5c91973cff72f54075",
+    "url": "/static/js/main.e0af77b6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "13cf1c1849c53dc42c0892b44b69cb76",
+    "url": "/static/media/1-meteor.13cf1c18.jpg"
+  }
+]);
